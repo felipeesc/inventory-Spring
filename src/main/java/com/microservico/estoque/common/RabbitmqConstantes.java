a46a33ca8e848456e49638a4d1100265bd3f65e7
Carvalho <felipee_sc@hotmail.com>
2021-09-03 18:12:34 -0300

    create queues reference rabbitmq

--- src/main/java/com/microservico/estoque/common/RabbitmqConstantes.java
+++ src/main/java/com/microservico/estoque/common/RabbitmqConstantes.java
@@ -2,7 +2,17 @@ package com.microservico.estoque.common;
 
 public class RabbitmqConstantes {
 
-    public static final String FILA_ESTOQUE = "ESTOQUE";
-    public static final String FILA_PRECO = "PRECO";
+    public static final String QUEUE_CATEGORY = "CATEGORY";
+    public static final String QUEUE_CITY = "CITY";
+    public static final String QUEUE_INPUT = "INPUT";
+    public static final String QUEUE_INPUT_ITEM = "INPUT-ITEM";
+    public static final String QUEUE_OUTPUT = "OUTPUT";
+    public static final String QUEUE_OUTPUT_ITEM = "OUTPUT-ITEM";
+    public static final String QUEUE_PRODUCT = "PRODUCT";
+    public static final String QUEUE_PROVIDER = "PROVIDER";
+    public static final String QUEUE_STORE = "STORE";
+    public static final String QUEUE_TOKEN = "TOKEN";
+    public static final String QUEUE_TRANSPORT = "TRANSPORT";
+    public static final String QUEUE_USER = "USER";
 
 }
--- src/main/java/com/microservico/estoque/configuration/RabbitMQConection.java
+++ src/main/java/com/microservico/estoque/configuration/RabbitMQConection.java
@@ -14,38 +14,83 @@ public class RabbitMQConection {
     private static final String NOME_EXCHANGE = "amq.direct";
 
     private AmqpAdmin amqpAdmin;
-    public RabbitMQConection(AmqpAdmin amqpAdmin){
+
+    public RabbitMQConection(AmqpAdmin amqpAdmin) {
         this.amqpAdmin = amqpAdmin;
     }
 
-    private Queue fila(String nomeFila){
+    private Queue fila(String nomeFila) {
         return new Queue(nomeFila, true, false, false);
     }
 
-    private DirectExchange trocaDireta(){
+    private DirectExchange trocaDireta() {
         return new DirectExchange(NOME_EXCHANGE);
     }
 
-    private Binding relacionamento(Queue fila, DirectExchange exchange){
-       return new Binding(fila.getName(), Binding.DestinationType.QUEUE, exchange.getName(), fila.getName(), null);
+    private Binding relacionamento(Queue fila, DirectExchange exchange) {
+        return new Binding(fila.getName(), Binding.DestinationType.QUEUE, exchange.getName(), fila.getName(), null);
     }
 
     @PostConstruct
-    private void adiciona(){
-        Queue filaEstoque = this.fila(RabbitmqConstantes.FILA_ESTOQUE);
-        Queue filaPreco = this.fila(RabbitmqConstantes.FILA_PRECO);
+    private void adiciona() {
+        Queue queueCategory = this.fila(RabbitmqConstantes.QUEUE_CATEGORY);
+        Queue queueCity = this.fila(RabbitmqConstantes.QUEUE_CITY);
+        Queue queueInput = this.fila(RabbitmqConstantes.QUEUE_INPUT);
+        Queue queueInputItem = this.fila(RabbitmqConstantes.QUEUE_INPUT_ITEM);
+        Queue queueOutput = this.fila(RabbitmqConstantes.QUEUE_OUTPUT);
+        Queue queueOutputItem = this.fila(RabbitmqConstantes.QUEUE_OUTPUT_ITEM);
+        Queue queueProduct = this.fila(RabbitmqConstantes.QUEUE_PRODUCT);
+        Queue queueProvider = this.fila(RabbitmqConstantes.QUEUE_PROVIDER);
+        Queue queueStore = this.fila(RabbitmqConstantes.QUEUE_STORE);
+        Queue queueToken = this.fila(RabbitmqConstantes.QUEUE_TOKEN);
+        Queue queueTransport = this.fila(RabbitmqConstantes.QUEUE_TRANSPORT);
+        Queue queueUser = this.fila(RabbitmqConstantes.QUEUE_USER);
+
 
         DirectExchange trocaDeFila = this.trocaDireta();
 
-        Binding ligacaoEstoque = this.relacionamento(filaEstoque, trocaDeFila);
-        Binding ligacaoPreco = this.relacionamento(filaPreco, trocaDeFila);
+        Binding callCategory = this.relacionamento(queueCategory, trocaDeFila);
+        Binding callCity = this.relacionamento(queueCity, trocaDeFila);
+        Binding callInput = this.relacionamento(queueInput, trocaDeFila);
+        Binding callInputItem = this.relacionamento(queueInputItem, trocaDeFila);
+        Binding callOutput = this.relacionamento(queueOutput, trocaDeFila);
+        Binding callOutputItem = this.relacionamento(queueOutputItem, trocaDeFila);
+        Binding callProduct = this.relacionamento(queueProduct, trocaDeFila);
+        Binding callProvider = this.relacionamento(queueProvider, trocaDeFila);
+        Binding callStore = this.relacionamento(queueStore, trocaDeFila);
+        Binding callToken = this.relacionamento(queueToken, trocaDeFila);
+        Binding callTransport = this.relacionamento(queueTransport, trocaDeFila);
+        Binding callUser = this.relacionamento(queueUser, trocaDeFila);
 
-        this.amqpAdmin.declareQueue(filaEstoque);
-        this.amqpAdmin.declareQueue(filaPreco);
+
+        this.amqpAdmin.declareQueue(queueCategory);
+        this.amqpAdmin.declareQueue(queueCity);
+        this.amqpAdmin.declareQueue(queueInput);
+        this.amqpAdmin.declareQueue(queueInputItem);
+        this.amqpAdmin.declareQueue(queueOutput);
+        this.amqpAdmin.declareQueue(queueOutputItem);
+        this.amqpAdmin.declareQueue(queueProduct);
+        this.amqpAdmin.declareQueue(queueProvider);
+        this.amqpAdmin.declareQueue(queueStore);
+        this.amqpAdmin.declareQueue(queueToken);
+        this.amqpAdmin.declareQueue(queueTransport);
+        this.amqpAdmin.declareQueue(queueUser);
+
 
         this.amqpAdmin.declareExchange(trocaDeFila);
 
-        this.amqpAdmin.declareBinding(ligacaoEstoque);
-        this.amqpAdmin.declareBinding(ligacaoPreco);
+        this.amqpAdmin.declareBinding(callCategory);
+        this.amqpAdmin.declareBinding(callCity);
+        this.amqpAdmin.declareBinding(callInput);
+        this.amqpAdmin.declareBinding(callInputItem);
+        this.amqpAdmin.declareBinding(callOutput);
+        this.amqpAdmin.declareBinding(callOutputItem);
+        this.amqpAdmin.declareBinding(callProduct);
+        this.amqpAdmin.declareBinding(callProvider);
+        this.amqpAdmin.declareBinding(callStore);
+        this.amqpAdmin.declareBinding(callToken);
+        this.amqpAdmin.declareBinding(callTransport);
+        this.amqpAdmin.declareBinding(callUser);
+
     }
 }
